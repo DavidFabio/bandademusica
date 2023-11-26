aceef154a4312595ca25be64e7c0dc803e49b642
DavidFabio <145555761+DavidFabio@users.noreply.github.com>
2023-11-26 22:46:46 +0100

    Update Participante.java

---
+++ src/dominio/Participante.java
@@ -0,0 +1,18 @@
+package dominio;
+
+public class Participante {
+    private String nombre;
+
+    public Participante(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+    public void annadirParticipante{
+
+        lista.add();
+    }
+}
